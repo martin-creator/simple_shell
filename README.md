e2a39d373a2703b84473965998f3d9559e0a2e50
martin-creator <martinlubowa@outlook.com>
2022-05-18 11:22:34 +0300

    Final

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-0x16. C - Simple Shell
+# simple_shell
+Project to be done in teams of 2 people (your team: Matthew Lawrence, Caleb Nyong
--- shell.c
+++ shell.c
@@ -1,87 +1,150 @@
-#include "shell.h"
+#include "holberton.h"
 
 /**
- * execute - executes the command
- * @cmd: command to run
- * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ * shell - simple shell
+ * @build: input build
  */
-int execute(char **cmd)
+void shell(config *build)
 {
-
-	pid_t child_pid;
-	int status;
-
-	if (strncmp("exit", cmd[0], 4) == 0)
-		return (-1);
-
-	child_pid = fork();
-
-	if (child_pid == -1)
+	while (true)
 	{
-		perror("Error");
-		return (1);
+		checkAndGetLine(build);
+		if (splitString(build) == false)
+			continue;
+		if (findBuiltIns(build) == true)
+			continue;
+		checkPath(build);
+		forkAndExecute(build);
 	}
-	else if (child_pid == 0)
-	{
-		if (execve(cmd[0], cmd, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
-	}
-	else
-		wait(&status);
-
-	return (0);
 }
 
+/**
+ * checkAndGetLine - check stdin and retrieves next line; handles
+ * prompt display
+ * @build: input build
+ */
+void checkAndGetLine(config *build)
+{
+	register int len;
+	size_t bufferSize = 0;
+	char *ptr, *ptr2;
+
+	build->args = NULL;
+	build->envList = NULL;
+	build->lineCounter++;
+	if (isatty(STDIN_FILENO))
+		displayPrompt();
+	len = getline(&build->buffer, &bufferSize, stdin);
+	if (len == EOF)
+	{
+		freeMembers(build);
+		if (isatty(STDIN_FILENO))
+			displayNewLine();
+		if (build->errorStatus)
+			exit(build->errorStatus);
+		exit(EXIT_SUCCESS);
+
+	}
+	ptr = _strchr(build->buffer, '\n');
+	ptr2 = _strchr(build->buffer, '\t');
+	if (ptr || ptr2)
+		insertNullByte(build->buffer, len - 1);
+	stripComments(build->buffer);
+}
 
 /**
- * main - main simple shell
- * @argc: number of arguments
- * @argv: list of command line arguments
- * Return: Always 0, -1 on error.
+ * stripComments - remove comments from input string
+ * @str: input string
+ * Return: length of remaining string
  */
-
-int main(int argc, char **argv)
+void stripComments(char *str)
 {
+	register int i = 0;
+	_Bool notFirst = false;
 
-	int response;
-	char **tokens;
-	size_t bufsize = BUFSIZ;
-	int isPipe = 0;
-	char *buffer;
-
-	if (argc >= 2)
+	while (str[i])
 	{
-		/*TODO: Handle cases where there is no argument, only the command*/
-		if (execve(argv[1], argv, NULL) == -1)
+		if (i == 0 && str[i] == '#')
 		{
-			perror("Error");
-			exit(-1);
+			insertNullByte(str, i);
+			return;
 		}
-		return (0);
+		if (notFirst)
+		{
+			if (str[i] == '#' && str[i - 1] == ' ')
+			{
+				insertNullByte(str, i);
+				return;
+			}
+		}
+		i++;
+		notFirst = true;
 	}
+}
 
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
+/**
+ * forkAndExecute - fork current build and execute processes
+ * @build: input build
+ */
+void forkAndExecute(config *build)
+{
+	int status;
+	pid_t f1 = fork();
+
+	convertLLtoArr(build);
+	if (f1 == -1)
 	{
-		perror("Unable to allocate buffer");
+		perror("error\n");
+		freeMembers(build);
+		freeArgs(build->envList);
 		exit(1);
 	}
-
-	do {
-		if (isatty(fileno(stdin)))
+	if (f1 == 0)
+	{
+		if (execve(build->fullPath, build->args, build->envList) == -1)
 		{
-			isPipe = 1;
-			_puts("cisfun#: ");
+			errorHandler(build);
+			freeMembers(build);
+			freeArgs(build->envList);
+			if (errno == ENOENT)
+				exit(127);
+			if (errno == EACCES)
+				exit(126);
 		}
+	} else
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			build->errorStatus = WEXITSTATUS(status);
+		freeArgsAndBuffer(build);
+		freeArgs(build->envList);
+	}
+}
 
-		getline(&buffer, &bufsize, stdin);
-		buffer[_strlen(buffer) - 1] = '\0';
-		tokens = stringToTokens(buffer);
-		response = execute(tokens);
-	} while (isPipe && response != -1);
+/**
+ * convertLLtoArr - convert linked list to array
+ * @build: input build
+ */
+void convertLLtoArr(config *build)
+{
+	register int i = 0;
+	size_t count = 0;
+	char **envList = NULL;
+	linked_l *tmp = build->env;
 
-	return (0);
+	count = list_len(build->env);
+	envList = malloc(sizeof(char *) * (count + 1));
+	if (!envList)
+	{
+		perror("Malloc failed\n");
+		exit(1);
+	}
+	while (tmp)
+	{
+		envList[i] = _strdup(tmp->string);
+		tmp = tmp->next;
+		i++;
+	}
+	envList[i] = NULL;
+	build->envList = envList;
 }
